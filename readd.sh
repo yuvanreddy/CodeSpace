77b58e0b7889cc51d44d823604d12f4c6c5b21f2
yuvanreddy <98659742+yuvanreddy@users.noreply.github.com>
2022-11-30 16:38:40 +0000

    Latest-Nov-30

--- readd.sh
+++ readd.sh
@@ -5,13 +5,14 @@ rm -rf report.txt
 read -p "Enter file name : " filename
 while read line
 do 
-curl --url "https://www.virustotal.com/vtapi/v2/url/report?apikey=d7cabc7be1d9d9f4ed2e1c74aa66cb24c1cab8e90b61595c04e32b996b25867c&resource=${line}" > report.txt
+curl --url "https://www.virustotal.com/vtapi/v2/url/report?apikey=c285ae41bd905acff2b39d68a626e7721986d3ba0e6fa1effdb696fbaa922d69&resource=${line}" > report.txt
 Clean=$(''tr ',' '[\n*]' < report.txt | grep -c -w '"result": "clean site"')
 Unrated=$(''tr ',' '[\n*]' < report.txt | grep -c -w '"result": "unrated site"')
 Phishing=$(''tr ',' '[\n*]' < report.txt | grep -c -w '"result": "phishing site"')
 Malicious="$Phishing"
 touch results.csv
 echo "$line","$Clean","$Unrated","$Phishing","$Malicious" > header.csv && cat results.csv >> header.csv && mv header.csv results.csv
+sleep 15
 done < $filename
 echo "Domain Name","Clean Sites","Unrated Sites","Phishing Sites","Malicious Sites" > header.csv && cat results.csv >> header.csv && mv header.csv results.csv
 cat results.csv
